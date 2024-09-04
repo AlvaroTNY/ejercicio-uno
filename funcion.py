bc9471e8245b2209d32e8156904048b4f8d04850
Alvaro <tapariona@gmail.com>
2024-09-04 10:25:16 -0500

    codigo modificado

--- funcion.py
+++ funcion.py
@@ -1,3 +1,6 @@
-def sum(*num):
-    return sum(num)
-print(sum(2,3,4,5,6,78,9,0,8))
+# def sum(*num):
+#     return sum(num)
+# print(sum(2,3,4,5,6,78,9,0,8))
+nombre = "soy"
+apellido = "pariona"
+result = f"Hola {nombre} {apellido}"
