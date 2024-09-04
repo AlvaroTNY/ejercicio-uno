3269653265cf978f364fb25e46c48c68127ad4bf
Alvaro <162912557+AlvaroTNY@users.noreply.github.com>
2024-09-04 16:17:34 -0500

    Update funcion.py

--- funcion.py
+++ funcion.py
@@ -3,4 +3,4 @@
 # print(sum(2,3,4,5,6,78,9,0,8))
 nombre = "soy"
 apellido = "pariona"
-result = f"Hola {nombre} {apellido} como estas"
+result = f"Hola {nombre} {apellido} como estas Yo muy bien y tu"
